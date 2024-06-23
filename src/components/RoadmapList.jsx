d9de6a428a397a4b45696616089d7b1515d1cd12
devtochit <71229278+devtochit@users.noreply.github.com>
2024-06-23 19:54:26 +0100

    feat: removed unused code

--- src/components/RoadmapList.jsx
+++ src/components/RoadmapList.jsx
@@ -1,48 +1,27 @@
-import { check } from "../assets";
-import { pricing } from "../constants";
-import Button from "./Button";
+import { check } from '../assets';
+import { pricing } from '../constants';
+import Button from './Button';
 
 const RoadmapList = () => {
   return (
-    <div className="flex gap-[1rem] max-lg:flex-wrap">
+    <div className='flex gap-[1rem] max-lg:flex-wrap'>
       {pricing.map((item) => (
         <div
           key={item.id}
-          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
+          className='w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3'
         >
-          {/* <h4 className="h4 mb-4">{item.title}</h4> */}
-
-          <p className="body-2 min-h-[4rem] mb-3 font-bold text-white text-2xl">
+          <p className='body-2  font-grotesk min-h-[4rem] mb-3 font-bold text-white text-2xl'>
             {item.description}
           </p>
-
-          {/* <div className="flex items-center h-[5.5rem] mb-6">
-            {item.price && (
-              <>
-                <div className="h3">$</div>
-                <div className="text-[5.5rem] leading-none font-bold">
-                  {item.price}
-                </div>
-              </>
-            )}
-          </div> */}
-
-          {/* <Button
-            className="w-full mb-6"
-            href={item.price ? "/pricing" : "mailto:contact@jsmastery.pro"}
-            white={!!item.price}
-          >
-            {item.price ? "Get started" : "Contact us"}
-          </Button> */}
 
           <ul>
             {item.features.map((feature, index) => (
               <li
                 key={index}
-                className="flex items-start py-5 border-t border-n-6"
+                className='flex items-start py-5 border-t border-n-6'
               >
                 {/* <img src={check} width={24} height={24} alt="Check" /> */}
-                <p className="body-2 ml-4 text-white">{feature}</p>
+                <p className='body-2 ml-4 text-white'>{feature}</p>
               </li>
             ))}
           </ul>
