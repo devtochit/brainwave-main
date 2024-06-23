078dd9d673ee804128c32de13437d92d80b20973
devtochit <71229278+devtochit@users.noreply.github.com>
2024-06-23 16:13:25 +0100

    feat: added new fonts

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -7,30 +7,30 @@ import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
-    <Section id="features">
-      <div className="container relative z-2">
-        <Heading className="md:max-w-md lg:max-w-2xl" title="Features" />
+    <Section id='features'>
+      <div className='container relative z-2'>
+        <Heading className='md:max-w-md lg:max-w-2xl' title='Features' />
 
-        <div className="flex flex-wrap gap-10 mb-10">
+        <div className='flex flex-wrap gap-10 mb-10'>
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className='block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]'
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h5 mb-5">{item.title}</h5>
-                <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
+              <div className='relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none'>
+                <h5 className='h5 mb-5 font-grotesk'>{item.title}</h5>
+                <p className='body-2 mb-6 text-n-3'>{item.text}</p>
+                <div className='flex items-center mt-auto'>
                   <img
                     src={item.iconUrl}
                     width={48}
                     height={48}
                     alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                  <p className='ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider'>
                     Explore more
                   </p>
                   <Arrow />
@@ -40,17 +40,17 @@ const Benefits = () => {
               {item.light && <GradientLight />}
 
               <div
-                className="absolute inset-0.5 bg-n-8"
-                style={{ clipPath: "url(#benefits)" }}
+                className='absolute inset-0.5 bg-n-8'
+                style={{ clipPath: 'url(#benefits)' }}
               >
-                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                <div className='absolute inset-0 opacity-0 transition-opacity hover:opacity-10'>
                   {item.imageUrl && (
                     <img
                       src={item.imageUrl}
                       width={380}
                       height={362}
                       alt={item.title}
-                      className="w-full h-full object-cover"
+                      className='w-full h-full object-cover'
                     />
                   )}
                 </div>
